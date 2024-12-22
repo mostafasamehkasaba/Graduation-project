26b4bc20ed6446b4ed78475fa2281531fb80eb72
mostafasamehkasaba <mostafakasaba8@gmail.com>
2024-12-22 22:35:08 +0200

    lcoal storage

--- script.js
+++ script.js
@@ -2,9 +2,17 @@ let userName = document.getElementById("userName");
 let password = document.getElementById("password");
 let btn = document.getElementById("btn");
 
-btn.onclick = function () {
+btn.addEventListener("click", function () {
+  if (userName.value === "" || password.value === "") {
+    alert("Please fill in all fields!");
+    return;
+  }
+
   localStorage.setItem("userName", userName.value);
   localStorage.setItem("password", password.value);
+
   window.location.href = "index.html";
+
   userName.value = "";
-};
+  password.value = "";
+});
