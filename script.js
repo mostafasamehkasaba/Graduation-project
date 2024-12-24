44424a9abe20d5519f7e873003f6adf38f647ce0
mostafasamehkasaba <mostafakasaba8@gmail.com>
2024-12-24 22:34:57 +0200

    edit in css and js

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-let userName = document.getElementById("userName");
-let password = document.getElementById("password");
-let btn = document.getElementById("btn");
+document.addEventListener("DOMContentLoaded", function () {
+  let userName = document.getElementById("userName");
+  let password = document.getElementById("password");
+  let btn = document.getElementById("btn");
 
-btn.addEventListener("click", function () {
-  if (userName.value === "" || password.value === "") {
-    alert("Please fill in all fields!");
-    return;
+  if (btn) {
+    btn.addEventListener("click", function () {
+      if (userName.value === "" || password.value === "") {
+        alert("Please fill in all fields!");
+        return;
+      }
+
+      localStorage.setItem("userName", userName.value);
+      localStorage.setItem("password", password.value);
+
+      userName.value = "";
+      password.value = "";
+
+      setTimeout(function () {
+        window.location.href = "index.html";
+      }, 1000);
+    });
+  } else {
   }
-
-  localStorage.setItem("userName", userName.value);
-  localStorage.setItem("password", password.value);
-
-  userName.value = "";
-  password.value = "";
-
-  setTimeout(function () {
-    window.location.href = "index.html";
-  }, 1000);
 });
