9af457a0da6a4ae9f3f0a1e27482a0bcd401bb97
mostafasamehkasaba <mostafakasaba8@gmail.com>
2024-12-29 20:45:06 +0200

    home page

--- script.js
+++ script.js
@@ -22,3 +22,19 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+
+var sun = document.getElementById("light");
+var moon = document.getElementById("dark");
+var body = document.body;
+
+sun.onclick = function changee() {
+  sun.style.display = "none";
+  moon.style.display = "block";
+  body.style.backgroundColor = "white";
+};
+
+moon.onclick = function restore() {
+  body.style.backgroundColor = "#181C14";
+  moon.style.display = "none";
+  sun.style.display = "block";
+};
